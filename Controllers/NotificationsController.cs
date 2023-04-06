16dd270de9b36bc3c9a5260b92ab260464ca1a15
LinaNieto <lina.nieto22243@ucaldas.edu.co>
2023-04-05 22:10:40 -0500

    Sending emails with templates using Sendgrid

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -9,24 +9,59 @@ namespace ms_notifications_akinmueble.Controllers;
 [Route("[controller]")]
 public class NotificationsController : ControllerBase
 {
-    [Route ("email")]
+    [Route ("welcome-email")]
     [HttpPost]
-    public async Task<ActionResult> SendEmail(EmailModel datos){
+    public async Task<ActionResult> SendWelcomeEmail(EmailModel datos){
 
         var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
         var client = new SendGridClient(apiKey);
-        var from = new EmailAddress("lina.nieto22243@ucaldas.edu.co", "Lina Nieto");
-        var subject = datos.emailSubject;
-        var to = new EmailAddress(datos.recipientEmail, datos.recipientName);
-        var plainTextContent = "plain text content";
-        var htmlContent = datos.emailBody;
-        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-        var response = await client.SendEmailAsync(msg);
 
+        SendGridMessage msg = this.CreateBaseMessage(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name=datos.recipientName,
+            message="Welcome to Akinmueble real estate."
+        });
+        var response = await client.SendEmailAsync(msg);
         if(response.StatusCode == System.Net.HttpStatusCode.Accepted){
             return Ok("Email sent to the address "+  datos.recipientEmail);
         }else{
             return BadRequest("Error sending the message to the address: " + datos.recipientEmail);
         }
+    }
+
+
+    [Route ("password-recovery-email")]
+    [HttpPost]
+    public async Task<ActionResult> SendPasswordRecoveryEmail(EmailModel datos){
+        
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+
+        SendGridMessage msg = this.CreateBaseMessage(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name=datos.recipientName,
+            message="This is your new password... Do not share it."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if(response.StatusCode == System.Net.HttpStatusCode.Accepted){
+            return Ok("Email sent to the address "+  datos.recipientEmail);
+        }else{
+            return BadRequest("Error sending the message to the address: " + datos.recipientEmail);
+        }
+    }
+
+    private SendGridMessage CreateBaseMessage(EmailModel datos){
+        
+        var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
+        var subject = datos.emailSubject;
+        var to = new EmailAddress(datos.recipientEmail, datos.recipientName);
+        var plainTextContent = "plain text content";
+        var htmlContent = datos.emailBody;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        return msg;
     }
 }
